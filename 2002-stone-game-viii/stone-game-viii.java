514cb84012608b2aef778345c245950a833b8d7b
kishore <bkishore27022006@gmail.com>
2026-08-25 18:14:04 +0530

    Time: 4 ms (67.09%) | Memory: 91.9 MB (43.59%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.java
+++ 2002-stone-game-viii/stone-game-viii.java
@@ -5,11 +5,10 @@ class Solution {
         for(int i=1;i<stones.length;i++){
             dp[i] = dp[i-1]+stones[i];
         }
-        int[] res = new int[stones.length];
-        res[stones.length-1] = dp[stones.length-1];
+        int max = dp[stones.length-1];
         for(int i=stones.length-2;i>=1;i--){
-            res[i] = Math.max(res[i+1],dp[i]-res[i+1]);
+            max = Math.max(max,dp[i]-max);
         }
-        return res[1];
+        return max;
     }
 }
